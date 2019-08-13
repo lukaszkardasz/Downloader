8e52999eac4c43079d93cd8e1b3270588ccf00d2
n2God <kardasz.lukasz@gmail.com>
2019-08-13 22:47:11 +0200

    init commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+/**
+ * @project Ddownloader
+ * @author n2god on 13/08/2019
+ */module Ddownloader {
+     requires org.jsoup;
+}
---
+++ src/main/java/pl/n2god/downloader/main/Downloader.java
@@ -0,0 +1,8 @@
+package pl.n2god.downloader.main;
+
+/**
+ * @author n2god on 13/08/2019
+ * @project Ddownloader
+ */
+public class Downloader {
+}
