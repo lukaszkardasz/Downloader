4f879cda9c1e84f51c06ee55516dae8cfb3a4749
lukaszkardasz <kardasz.lukasz@gmail.com>
2019-08-14 16:26:37 +0200

    show article titles from joemonster.org

--- src/main/java/pl/n2god/downloader/main/Downloader.java
+++ src/main/java/pl/n2god/downloader/main/Downloader.java
@@ -1,8 +1,28 @@
 package pl.n2god.downloader.main;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.URL;
+
 /**
  * @author n2god on 13/08/2019
  * @project Ddownloader
  */
 public class Downloader {
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://www.joemonster.org");
+
+        Document document = Jsoup.parse(url, 3000);
+        Elements divElements = document.select("div");
+        for (Element div : divElements) {
+            String divText = div.getElementsByClass("indexart").text();
+            if (!divText.isEmpty()){
+                System.out.println(divText);
+            }
+        }
+    }
 }
